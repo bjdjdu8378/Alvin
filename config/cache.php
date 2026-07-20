4cd9c4f64dffae61eda498435ec94cdf04b20a47
bjdjdu8378 <sakino.umbrael@gmail.com>
2026-07-20 18:33:45 +0200

    feat: Add Laravel core configuration

---
+++ config/cache.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'default' => env('CACHE_DRIVER', 'redis'),
+    'stores' => [
+        'array' => [
+            'driver' => 'array',
+            'serialize' => false,
+        ],
+        'database' => [
+            'driver' => 'database',
+            'connection' => null,
+            'table' => 'cache',
+        ],
+        'file' => [
+            'driver' => 'file',
+            'path' => storage_path('framework/cache/data'),
+        ],
+        'memcached' => [
+            'driver' => 'memcached',
+            'servers' => [
+                '127.0.0.1:11211',
+            ],
+        ],
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+            'lock_connection' => 'default',
+        ],
+    ],
+    'prefix' => env('CACHE_PREFIX', 'alvin_cache'),
+];
